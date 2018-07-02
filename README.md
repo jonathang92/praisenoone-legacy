ecf826b9fb60220b964c78befba15dc489fc9f1e
Jonathan Alexander González <jonathan.g92r@gmail.com>
2018-07-02 17:05:44 -0300

    Carga de página

---
+++ README.md
@@ -0,0 +1,5 @@
+# Praise No One - Web Page
+
+Repositorio público de la página de la banda.
+
+[Link a la página](https://jonathang92.github.io/praisenoone/)
---
+++ js/main.js
@@ -0,0 +1,18 @@
+$("body").niceScroll({
+    cursorcolor:		"#000000c9",
+    cursorwidth: 		"12px",
+    cursorborder: 		"0px solid #000",
+    scrollspeed: 		60,
+    autohidemode: 		true,
+    background: 		'#ffffff96',
+    cursorborderradius: 0,
+    cursorfixedheight: 	false,
+    cursorminheight: 	20,
+    enablekeyboard: 	true,
+    horizrailenabled: 	true,
+    bouncescroll: 		false,
+    smoothscroll: 		true,
+    iframeautoresize: 	true,
+    touchbehavior: 		false,
+    zindex: 999
+});
